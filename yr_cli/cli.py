6809f58aa840286180936b4c220772d67f5691ac
Tim Wolff-Piggott <twolffpiggott@gmail.com>
2024-09-15 16:01:10 +0200

    refactor weekend time series logic

--- yr_cli/cli.py
+++ yr_cli/cli.py
@@ -222,6 +222,7 @@ def summary(
     if not selected_location:
         return
 
+    # yr only gives medium-term summary forecasts at defined 6-hour increments of UTC time
     now_dt = datetime.now().astimezone()
     utc_now = now_dt.astimezone(timezone.utc)
     utc_start_time = utc_now.replace(minute=0, second=0, microsecond=0) + timedelta(
@@ -268,28 +269,34 @@ def weekend(
         return
 
     now_dt = datetime.now().astimezone()
-    start_time = now_dt.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
-    current_day = now_dt.date()
-    days_ahead = 4 - current_day.weekday()
-    if days_ahead <= 0:
-        days_ahead += 7
-    start_time = start_time + timedelta(days=days_ahead)
-    utc_start_time = start_time.astimezone(timezone.utc).replace(
-        minute=0, second=0, microsecond=0
+    utc_now = now_dt.astimezone(timezone.utc)
+    days_to_friday = 4 - now_dt.weekday()
+    if days_to_friday <= 0:
+        days_to_friday += 7
+    start_time = (
+        now_dt.replace(minute=0, second=0, microsecond=0)
+        + timedelta(hours=1)
+        + timedelta(days=days_to_friday)
     )
-    utc_start_hour = utc_start_time.hour
+    utc_start_time = (
+        utc_now.replace(minute=0, second=0, microsecond=0)
+        + timedelta(hours=1)
+        + timedelta(days=days_to_friday)
+    )
     time_series = []
+    # yr only gives medium-term summary forecasts at defined 6-hour increments of UTC time
+    # if more granular data is available (within the next two days), use it
     utc_summary_hours = [0, 6, 12, 18]
+    # fri, sat, sun
     for day in range(3):
-        if days_ahead <= 2:
-            if day == 0:
-                hours = [utc_start_hour] + [
-                    hour for hour in range(24) if hour > utc_start_hour
-                ]
+        if days_to_friday <= 2:
+            granular_local_hours = range(24)
+            if days_to_friday == 0:
+                granular_local_hours = range(start_time.hour, 24)
             hours_for_day = [
                 start_time.replace(hour=hour, minute=0, second=0, microsecond=0)
                 + timedelta(days=day)
-                for hour in hours
+                for hour in granular_local_hours
             ]
         else:
             hours_for_day = [
